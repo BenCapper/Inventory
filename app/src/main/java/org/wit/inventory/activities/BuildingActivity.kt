5d0e18f271c9e09ba5878fac161b7e55e4499520
Ben Capper <Bencapper19@gmail.com>
2021-03-23 13:15:29 +0000

    Added thumbnail to building list
    changed theme
    lists properly update
    set location button

--- app/src/main/java/org/wit/inventory/activities/BuildingActivity.kt
+++ app/src/main/java/org/wit/inventory/activities/BuildingActivity.kt
@@ -1,5 +1,6 @@
 package org.wit.inventory.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,6 +10,9 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.inventory.R
+import org.wit.inventory.helpers.readImage
+import org.wit.inventory.helpers.readImageFromPath
+import org.wit.inventory.helpers.showImagePicker
 import org.wit.inventory.main.MainApp
 import org.wit.inventory.models.BuildingModel
 
@@ -17,6 +21,7 @@ class BuildingActivity : AppCompatActivity(), AnkoLogger {
     var building = BuildingModel()
     lateinit var app: MainApp
     var edit = false
+    val IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,10 @@ class BuildingActivity : AppCompatActivity(), AnkoLogger {
 
         if (intent.hasExtra("building_edit")) {
             building = intent.extras?.getParcelable<BuildingModel>("building_edit")!!
+            buildingImage.setImageBitmap(readImageFromPath(this, building.image))
+            if (building.image != null) {
+                chooseImage.setText(R.string.change_building_image)
+            }
             btnAdd.setText(R.string.save_building)
             buildingName.setText(building.name)
             buildingAddress.setText(building.address)
@@ -48,6 +57,14 @@ class BuildingActivity : AppCompatActivity(), AnkoLogger {
             setResult(AppCompatActivity.RESULT_OK)
             finish()
         }
+
+        chooseImage.setOnClickListener {
+            showImagePicker(this, IMAGE_REQUEST)
+        }
+
+        buildingLocation.setOnClickListener {
+            info ("Set Location Pressed")
+        }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_building, menu)
@@ -61,5 +78,17 @@ class BuildingActivity : AppCompatActivity(), AnkoLogger {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    building.image = data.getData().toString()
+                    buildingImage.setImageBitmap(readImage(this, resultCode, data))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/org/wit/inventory/models/BuildingModel.kt
+++ app/src/main/java/org/wit/inventory/models/BuildingModel.kt
@@ -5,5 +5,6 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class BuildingModel(var id: Long = 0,
                          var name: String = "",
-                         var address: String = "") : Parcelable {
+                         var address: String = "",
+                         var image: String = "") : Parcelable {
 }
